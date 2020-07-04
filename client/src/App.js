094242be68ccaebf0d561abb0cc125c912ba158f
Justin Marchi <marchij86@gmail.com>
2020-07-04 11:09:01 -0400

    adding activity template

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
+import FavoritesList from "./pages/FavoritesList";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+function App() {
+  return (
+    <Router>
+      <div>
+        <StoreProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/favorites" component={FavoritesList} />
+            <Route exact path="/posts/:id" component={Detail} />
+            <Route component={NoMatch} />
+          </Switch>
+        </StoreProvider>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
