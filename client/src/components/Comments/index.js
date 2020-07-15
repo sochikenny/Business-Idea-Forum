06823c1b56eb1584cd725d7c8682317fc51b8821
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-07-14 20:12:40 -0400

    adding more comment stuff

--- client/src/components/Comments/index.js
+++ client/src/components/Comments/index.js
@@ -1,20 +1,63 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_COMMENT, LOADING } from "../../utils/actions";
+import API from "../../utils/API";
 
-function Comments() {
+function Comments({postId}) {
+  const bodyRef = useRef();
+  const authorRef = useRef();
+  const [state, dispatch] = useStoreContext();
 
-    return (
-      <div>
-        <section>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: LOADING });
+    API.saveComment({
+      postId,
+      body: bodyRef.current.value,
+      author: bodyRef.current.value,
+    })
+      .then((result) => {
+        dispatch({
+          type: ADD_COMMENT,
+          comment: result.data,
+        });
+      })
+      .catch((err) => console.log(err));
+
+    bodyRef.current.value = "";
+    authorRef.current.value = "";
+  };
+
+  return (
+    <div>
+      <section>
         <h2>Leave a Comment:</h2>
-        <form className="form-group mt-5 mb-5" id="comment">
-          <textarea className="form-control mb-5" placeholder="body" />
-          <button className="btn btn-success mt-3 mb-5" type="submit">
-            Save Post
+        <form
+          className="form-group mt-5 mb-5"
+          id="comment"
+          onSubmit={handleSubmit}
+        >
+          <textarea
+            className="form-control mb-5"
+            ref={bodyRef}
+            placeholder="Body"
+          />
+          <input
+            className="form-control mb-5"
+            ref={authorRef}
+            placeholder="Screen name"
+          />
+          <button
+            className="btn btn-success mt-3 mb-5"
+            disabled={state.loading}
+            type="submit"
+          >
+            Submit
           </button>
         </form>
-        </section>
-      </div>
-    );
-  }
-  
-  export default Comments;
+      </section>
+    </div>
+  );
+}
+
+export default Comments;
