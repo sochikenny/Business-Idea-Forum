c89e0ae1d1660504eed08e287a5ed4a74b5dbe4c
Justin Marchi <marchij86@gmail.com>
2020-07-16 19:54:01 -0400

    Styling done for navbar and login page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,9 +5,10 @@ function Nav() {
   const [store] = useStoreContext();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/home">
-        Business Ideas Forum
+    <nav className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand mx-auto align-middle NavbarTitle">Business Ideas Forum</a>
+      <a class="btn btn-success" href="/" role="button">
+        Logout
       </a>
       {store.loading ? <a className="navbar-brand ml-auto">Loading...</a> : <></>}
     </nav>
